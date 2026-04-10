814f08d5eaeb5f35697898cff9131d4372066a91
Athul prakash <athultechnovia@gmail.com>
2026-04-10 12:49:57 +0530

    events fetching completed

--- android/app/src/main/kotlin/com/example/sync_fusion_calendar/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sync_fusion_calendar/MainActivity.kt
@@ -5,8 +5,10 @@ import android.provider.CalendarContract
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import java.util.*
 
 class MainActivity: FlutterActivity() {
+
     private val CHANNEL = "calendar_channel"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -16,41 +18,47 @@ class MainActivity: FlutterActivity() {
             .setMethodCallHandler { call, result ->
 
                 if (call.method == "getEvents") {
-
-                    val events = mutableListOf<Map<String, String>>()
-
-                    val cursor: Cursor? = contentResolver.query(
-                        CalendarContract.Events.CONTENT_URI,
-                        arrayOf(
-                            CalendarContract.Events.TITLE,
-                            CalendarContract.Events.DTSTART,
-                            CalendarContract.Events.DTEND
-                        ),
-                        null,
-                        null,
-                        null
-                    )
-
-                    cursor?.use {
-                        while (it.moveToNext()) {
-                            val title = it.getString(0)
-                            val start = it.getLong(1)
-                            val end = it.getLong(2)
-
-                            events.add(
-                                mapOf(
-                                    "title" to title,
-                                    "start" to start.toString(),
-                                    "end" to end.toString()
-                                )
-                            )
-                        }
-                    }
-
+                    val events = getCalendarEvents()
                     result.success(events)
                 } else {
                     result.notImplemented()
                 }
             }
+    }
+
+    private fun getCalendarEvents(): List<Map<String, Any?>> {
+        val eventList: MutableList<Map<String, Any?>> = ArrayList()
+
+        val projection = arrayOf(
+            CalendarContract.Events.TITLE,
+            CalendarContract.Events.DTSTART,
+            CalendarContract.Events.DTEND
+        )
+
+        val cursor: Cursor? = contentResolver.query(
+            CalendarContract.Events.CONTENT_URI,
+            projection,
+            null,
+            null,
+            null
+        )
+
+        cursor?.use {
+            val titleIndex = it.getColumnIndex(CalendarContract.Events.TITLE)
+            val startIndex = it.getColumnIndex(CalendarContract.Events.DTSTART)
+            val endIndex = it.getColumnIndex(CalendarContract.Events.DTEND)
+
+            while (it.moveToNext()) {
+                val event: MutableMap<String, Any?> = HashMap()
+
+                event["title"] = it.getString(titleIndex)
+                event["start"] = it.getLong(startIndex).toString()
+                event["end"] = it.getLong(endIndex).toString()
+
+                eventList.add(event)
+            }
+        }
+
+        return eventList
     }
 }
